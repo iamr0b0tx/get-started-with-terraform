ddd05e94794a64086f365aee94687032f3c01d76
iamr0b0tx <abduladeneye@gmail.com>
2021-05-19 05:20:49 +0100

    successfully deployed and example ec2 instance

---
+++ scripts/linux/aws-init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export AWS_ACCESS_KEY_ID=your-access-key-id
+export AWS_SECRET_ACCESS_KEY=your-secret-access-key
---
+++ scripts/windows/aws-init.bat
@@ -0,0 +1,2 @@
+set AWS_ACCESS_KEY_ID=your-access-key-id
+set AWS_SECRET_ACCESS_KEY=your-secret-access-key
