436ba0daa57a3495d606368f2e3770dc3c1dcc1c
iamr0b0tx <abduladeneye@gmail.com>
2021-05-19 06:18:01 +0100

    created and refactored scripts with example versions

---
+++ aws-init.example.bat
@@ -0,0 +1,2 @@
+set AWS_ACCESS_KEY_ID=aws-key-id
+set AWS_SECRET_ACCESS_KEY=aws-secret-key
--- scripts/windows/aws-init.bat
+++
@@ -1,2 +0,0 @@
-set AWS_ACCESS_KEY_ID=your-access-key-id
-set AWS_SECRET_ACCESS_KEY=your-secret-access-key
